c2554f20b18af3d90e405716160b2e8ef5543bd8
David Charte <fdavidcl@outlook.com>
2016-03-15 21:15:23 +0100

    Reorganize and prettify output

--- profiling.rb
+++ profiling.rb
@@ -8,15 +8,21 @@ mlibras = FeatureSelection::Dataset.read_arff("../../movement_libras.arff")
 wdbc = FeatureSelection::Dataset.read_arff("../../wdbc.arff", 0)
 iris = FeatureSelection::Dataset.data("iris")
 
-mlibras.tap do |dataset|
+iris.tap do |dataset|
   puts "Using #{dataset}"
 
-  sfs = FeatureSelection::SeqForwardSelection.new dataset
-  ls = FeatureSelection::LocalSearch.new dataset
+  heuristics = {
+    "Sequential Forward Selection" => FeatureSelection::SequentialSelection.new(dataset),
+    "Sequential Backward Selection" => FeatureSelection::SequentialSelection.new(dataset, false),
+    "First-descent Local Search" => FeatureSelection::FirstDescent.new(dataset)
+  }
 
   # profile the code
   #result = RubyProf.profile do
-    puts sfs.run
+  heuristics.each do |name, heuristic|
+    puts "\n#{"-" * 80}\n|#{name.center(78)}|\n#{"-" * 80}"
+    puts "Solution found: #{heuristic.run.join " with fitness "}"
+  end
   #end
 end
 
