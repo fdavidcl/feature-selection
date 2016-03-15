da3c4e7c77967c8260c5d825f139e10cddd8776d
David Charte <fdavidcl@outlook.com>
2016-03-15 01:48:53 +0100

    Use correct datasets

--- profiling.rb
+++ profiling.rb
@@ -3,16 +3,23 @@ require 'ruby-prof'
 require "bundler/setup"
 require "feature-selection"
 
-emotions = FeatureSelection::Dataset.read_arff("../../emotions.arff")
+arr = FeatureSelection::Dataset.read_arff("../../arrhythmia.arff")
+mlibras = FeatureSelection::Dataset.read_arff("../../movement_libras.arff")
+wdbc = FeatureSelection::Dataset.read_arff("../../wdbc.arff", 0)
 iris = FeatureSelection::Dataset.data("iris")
-sfs = FeatureSelection::SeqForwardSelection.new emotions
-ls = FeatureSelection::LocalSearch.new emotions
 
-# profile the code
-result = RubyProf.profile do
-  sfs.run
+mlibras.tap do |dataset|
+  puts "Using #{dataset}"
+
+  sfs = FeatureSelection::SeqForwardSelection.new dataset
+  ls = FeatureSelection::LocalSearch.new dataset
+
+  # profile the code
+  #result = RubyProf.profile do
+    puts sfs.run
+  #end
 end
 
 # print a graph profile to text
-printer = RubyProf::GraphPrinter.new(result)
-printer.print(STDOUT, {})
+# printer = RubyProf::GraphPrinter.new(result)
+# printer.print(STDOUT, {})
