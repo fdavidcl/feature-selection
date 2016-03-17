87fb7a495e70b89dd3e7fb026c5e70e8aaba8bac
David Charte <fdavidcl@outlook.com>
2016-03-17 02:04:18 +0100

    Prevent creating full neighborhood for solution

--- lib/feature-selection/heuristics/max_descent.rb
+++ lib/feature-selection/heuristics/max_descent.rb
@@ -3,7 +3,7 @@ require_relative "local_search"
 module FeatureSelection
   class MaximumDescent < LocalSearch
     def select_next
-      neighbor, fitness = neighborhood.to_a.max_by &:last
+      neighbor, fitness = neighborhood.max_by &:last
       [neighbor, fitness] if fitness > @fitness # returns nil otherwise
     end
   end
