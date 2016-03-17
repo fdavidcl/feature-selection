d82e7c3b96da522fd083840bad6dce4f7bffd165
David Charte <fdavidcl@outlook.com>
2016-03-17 01:55:31 +0100

    Minor change in first descent

--- lib/feature-selection/heuristics/first_descent.rb
+++ lib/feature-selection/heuristics/first_descent.rb
@@ -3,7 +3,7 @@ require_relative "local_search"
 module FeatureSelection
   class FirstDescent < LocalSearch
     def select_next
-      neighborhood.detect do |(attempt, fitness)|
+      neighborhood.detect do |attempt, fitness|
         fitness > @fitness
       end
     end
