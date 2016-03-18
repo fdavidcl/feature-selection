0b62a10d3c37daeb967b1545152c679fee5663e8
David Charte <fdavidcl@outlook.com>
2016-03-18 03:47:05 +0100

    Profiling/test script

--- profiling.rb
+++ profiling.rb
@@ -8,13 +8,20 @@ mlibras = FeatureSelection::Dataset.read_arff("../../movement_libras.arff")
 wdbc = FeatureSelection::Dataset.read_arff("../../wdbc.arff", 0)
 iris = FeatureSelection::Dataset.data("iris")
 
-iris.tap do |dataset|
-  puts "Using #{dataset}"
+arr.tap do |dataset|
+  puts "Using #{dataset}. Let's partition:"
+
+  partitions = (1..5).map{ dataset.partition 2 }
+
+  # Falta correr el kNN directamente sobre el dataset
 
   heuristics = {
-    "Sequential Forward Selection" => FeatureSelection::SequentialSelection.new(dataset),
-    "Sequential Backward Selection" => FeatureSelection::SequentialSelection.new(dataset, false),
-    "First-descent Local Search" => FeatureSelection::FirstDescent.new(dataset)
+    "No selection (kNN with all features)" => FeatureSelection::NoSelection.new(dataset),
+    #{}"Sequential Forward Selection" => FeatureSelection::SequentialSelection.new(dataset),
+    #{}"Sequential Backward Selection" => FeatureSelection::SequentialSelection.new(dataset, false),
+    "First-descent Local Search" => FeatureSelection::FirstDescent.new(dataset),
+    "Maximum-descent Local Search" => FeatureSelection::MaximumDescent.new(dataset)
+    #{}"Simulated Annealing" => FeatureSelection::SimAnnealing.new(dataset)
   }
 
   # profile the code
@@ -24,8 +31,8 @@ iris.tap do |dataset|
     puts "Solution found: #{heuristic.run.join " with fitness "}"
   end
   #end
-end
 
-# print a graph profile to text
-# printer = RubyProf::GraphPrinter.new(result)
-# printer.print(STDOUT, {})
+  # print a graph profile to text
+  # printer = RubyProf::GraphPrinter.new(result)
+  # printer.print(STDOUT, {})
+end
