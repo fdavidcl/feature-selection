7cfd09a5e9b0db6d1dbcfaec2e93c53eb27fda20
David Charte <fdavidcl@outlook.com>
2016-03-18 20:48:38 +0100

    Add evaluation class

---
+++ lib/feature-selection/evaluator.rb
@@ -0,0 +1,26 @@
+require_relative "heuristics/heuristic"
+require_relative "dataset"
+
+module FeatureSelection
+  class Evaluator
+    attr_accessor :folds, :repeats
+
+    def initialize folds: 2, repeats: 5
+      @folds = folds
+      @repeats = repeats
+    end
+
+    def evaluate heuristic_class, dataset
+      partitions = (0 ... repeats).map{ dataset.partition 2, random: FeatureSelection::RNG }
+      results = (partitions + partitions.map(&:reverse)).map do |train, test|
+        heuristic = heuristic_class.new(train)
+        solution, fitness = heuristic.run
+        evaluation = heuristic.classifier.class.new(NUMBER_NEIGHBORS, test).fitness_for(solution.ones)
+
+        [solution, fitness, evaluation]
+      end
+
+      ["Solution", "Training fitness", "Test fitness"].zip(results.transpose)
+    end
+  end
+end
