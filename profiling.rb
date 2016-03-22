bdfa0c4d5761949b7e3ea3b00a6eafdfdd565d79
David Charte <fdavidcl@outlook.com>
2016-03-22 13:44:59 +0100

    Adapt profiler script

--- profiling.rb
+++ profiling.rb
@@ -9,30 +9,43 @@ wdbc = FeatureSelection::Dataset.read_arff("../../wdbc.arff", 0)
 iris = FeatureSelection::Dataset.data("iris")
 
 arr.tap do |dataset|
-  puts "Using #{dataset}. Let's partition:"
+  puts "Using #{dataset}."
 
-  partitions = (1..5).map{ dataset.partition 2 }
+  # result = RubyProf.profile do
+  # partitions = (1..5).map{ dataset.partition 2, random: FeatureSelection::RNG }
+  # end
 
   # Falta correr el kNN directamente sobre el dataset
 
   heuristics = {
-    "No selection (kNN with all features)" => FeatureSelection::NoSelection.new(dataset),
-    #{}"Sequential Forward Selection" => FeatureSelection::SequentialSelection.new(dataset),
-    #{}"Sequential Backward Selection" => FeatureSelection::SequentialSelection.new(dataset, false),
-    "First-descent Local Search" => FeatureSelection::FirstDescent.new(dataset),
-    "Maximum-descent Local Search" => FeatureSelection::MaximumDescent.new(dataset)
-    #{}"Simulated Annealing" => FeatureSelection::SimAnnealing.new(dataset)
+    "No selection (kNN with all features)" => FeatureSelection::NoSelection,
+    #"Sequential Forward Selection" => FeatureSelection::SeqForwardSelection,
+    #"Sequential Backward Selection" => FeatureSelection::SeqBackwardSelection,
+    #"First-descent Local Search" => FeatureSelection::FirstDescent,
+    #"Maximum-descent Local Search" => FeatureSelection::MaximumDescent,
+    #"Simulated Annealing" => FeatureSelection::SimAnnealing,
+    #"Basic Tabu Search" => FeatureSelection::BasicTabuSearch,
+    "Complete Tabu Search" => FeatureSelection::TabuSearch
   }
+
+  ev = FeatureSelection::Evaluator.new folds: 2, repeats: 1
 
   # profile the code
   #result = RubyProf.profile do
   heuristics.each do |name, heuristic|
-    puts "\n#{"-" * 80}\n|#{name.center(78)}|\n#{"-" * 80}"
-    puts "Solution found: #{heuristic.run.join " with fitness "}"
+    puts "", "-" * 80, "|#{name.center(78)}|", "-" * 80
+    t1 = Time.now
+    results = ev.evaluate heuristic, dataset
+    t2 = Time.now
+    puts "-" * 80, "Summary".center(80)
+    puts "Fitness in training (mean): #{results[:training].reduce(&:+)/results[:training].length}"
+    puts "Fitness in test (mean): #{results[:test].reduce(&:+)/results[:test].length}"
+    puts "Reduction ratio: #{results[:reduction].reduce(&:+)/results[:reduction].length}"
+    puts "Time spent: total #{results[:time].reduce(&:+)}, mean #{results[:time].reduce(&:+)/results[:time].length}"
   end
   #end
 
   # print a graph profile to text
-  # printer = RubyProf::GraphPrinter.new(result)
-  # printer.print(STDOUT, {})
+  #printer = RubyProf::GraphPrinter.new(result)
+  #printer.print(STDOUT, {})
 end
