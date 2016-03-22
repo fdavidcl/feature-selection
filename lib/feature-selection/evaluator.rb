092465e3a7b07bf8c45eac4eaa2dd9e7e1c4b5e0
David Charte <fdavidcl@outlook.com>
2016-03-22 13:44:13 +0100

    Add functionality to evaluator

--- lib/feature-selection/evaluator.rb
+++ lib/feature-selection/evaluator.rb
@@ -10,17 +10,26 @@ module FeatureSelection
       @repeats = repeats
     end
 
-    def evaluate heuristic_class, dataset
-      partitions = (0 ... repeats).map{ dataset.partition 2, random: FeatureSelection::RNG }
+    def evaluate heuristic_class, dataset, csv: false
+      partitions = (0 ... repeats).map{ dataset.partition @folds, random: FeatureSelection::RNG }
       results = (partitions + partitions.map(&:reverse)).map do |train, test|
         heuristic = heuristic_class.new(train)
+        start = Time.now
         solution, fitness = heuristic.run
+        finish = Time.now
         evaluation = heuristic.classifier.class.new(NUMBER_NEIGHBORS, test).fitness_for(solution)
+        reduction = solution.count(0).to_f/solution.length
 
-        [solution, fitness, evaluation]
+        [solution, fitness, evaluation, reduction, finish - start]
       end
 
-      [:solution, :training, :test].zip(results.transpose).to_h
+      names = [:solution, :training, :test, :reduction, :time]
+
+      if csv
+        results.prepend(names).map{ |row| row.join ", " }.join("\n")
+      else
+        names.zip(results.transpose).to_h
+      end
     end
   end
 end
