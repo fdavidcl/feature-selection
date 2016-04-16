4795d3191e6ce808ff88c568ab955c82fc97e3d5
David Charte <fdavidcl@outlook.com>
2016-04-16 02:30:46 +0200

    Implement GRASP

--- lib/feature-selection/heuristics/grasp.rb
+++ lib/feature-selection/heuristics/grasp.rb
@@ -1,7 +1,18 @@
 require_relative "randomized_sfs"
 
 module FeatureSelection
-  class Grasp < LocalSearch
+  class Grasp < FirstDescent
+    def initialize dataset, debug: false, random: Random.new(CONFIG.random_seed)
+      super
 
+      @initial_generator = RandomizedSFS.new @dataset, debug: @debug, random: @rng
+    end
+
+    def outer_loop
+      CONFIG.grasp[:initial].times do
+        self.solution = @initial_generator.run
+        basic_local_search
+      end
+    end
   end
 end
--- lib/feature-selection/heuristics/randomized_sfs.rb
+++ lib/feature-selection/heuristics/randomized_sfs.rb
@@ -2,6 +2,18 @@ require_relative "greedy"
 
 module FeatureSelection
   class RandomizedSFS < SeqForwardSelection
+    private
+    def select_next
+      ranking = neighborhood.sort_by do |_, fitness|
+        fitness
+      end.reverse!
+      threshold = ranking.first[2] + CONFIG.grasp[:alpha] * (ranking.first[2] - ranking.last[2])
 
+      ranking.select! do |_, fitness|
+        fitness > threshold
+      end
+
+      ranking.sample random: @rng
+    end
   end
 end
