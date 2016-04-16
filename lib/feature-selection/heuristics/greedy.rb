19066b437cd69a6b190cceb209510571769dd8ad
David Charte <fdavidcl@outlook.com>
2016-04-16 02:30:39 +0200

    Improve Greedy implementation

--- lib/feature-selection/heuristics/greedy.rb
+++ lib/feature-selection/heuristics/greedy.rb
@@ -5,22 +5,23 @@ module FeatureSelection
   class SequentialSelection < Heuristic
     def initialize dataset, forward, debug: false, random: Random.new(CONFIG.random_seed)
       super(dataset, debug: debug, random: random)
-
-      @solution = []
-      @fitness = 0
-      @remaining = [* 0 ... @dataset.input_count]
+      
       @forward = forward
     end
 
     def run
+      @solution = []
+      @remaining = [* 0 ... @dataset.input_count]
+      @fitness = 0
+
       improving = true
 
       while improving do
-        feature, fitness = select_next
+        attempt, fitness, feature = select_next
 
-        if feature
+        if !fitness.nil? && fitness > @fitness
           puts "Next feature selected: #{feature} with fitness #{fitness}" if @debug
-          @solution << feature
+          @solution = attempt
           @remaining.delete feature
           @fitness = fitness
           improving = false if !@forward && @remaining.length == 1
@@ -37,16 +38,21 @@ module FeatureSelection
 
     private
     def select_next
-      @remaining.reduce([false, @fitness]) do |(best, fitness), feature|
-        next_attempt = @forward ? @solution + [feature] : @remaining - [feature]
-        # Evaluate current feature
-        new_fitness = fitness_for(next_attempt.to_bitarray(@dataset.input_count))
+      neighborhood.max_by do |_, fitness|
+        fitness
+      end
+    end
 
-        # Choose the best feature
-        if new_fitness > fitness
-          [feature, new_fitness]
-        else
-          [best, fitness]
+    def neighborhood
+      Enumerator.new do |yielder|
+        @remaining.each do |feature|
+          attempt = @forward ? @solution + [feature] : @remaining - [feature]
+
+          yielder << [
+            attempt,
+            fitness_for(attempt.to_bitarray(@dataset.input_count)),
+            feature
+          ]
         end
       end
     end
