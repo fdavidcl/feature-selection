ce1811992c7b7079420ae432992d7451ee95b75f
David Charte <fdavidcl@outlook.com>
2016-04-18 00:37:06 +0200

    Crossover operator

--- lib/feature-selection/heuristics/genetic.rb
+++ lib/feature-selection/heuristics/genetic.rb
@@ -6,8 +6,17 @@ module FeatureSelection
       super
 
       # initialize population
-      @population = (0 ... CONFIG.genetic[:size]).map{ random_solution }
+      @endless_forms = (0 ... CONFIG.genetic[:size]).map{ random_solution }
       @fitness = @population.map{ |solution| [solution, fitness_for(solution)] }.to_h
+    end
+
+    def crossover one, other
+      len = one.length
+      start, stop = [@rnd.rand(0 ... len), @rnd.rand(0 ... len)].sort
+
+      [[one, other], [other, one]].map do |pair|
+        pair[0][0 ... start] + pair[1][start ... stop] + pair[0][stop ... len]
+      end
     end
   end
 end
