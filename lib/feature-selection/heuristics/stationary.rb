ed2e6f136d8c84102f58d90b87fa98163743e3fb
David Charte <fdavidcl@outlook.com>
2016-05-01 16:42:14 +0200

    Stationary genetic

--- lib/feature-selection/heuristics/stationary.rb
+++ lib/feature-selection/heuristics/stationary.rb
@@ -5,31 +5,22 @@ module FeatureSelection
     private
     def generation
       best_child, worst_child = evolution
-      chromosome_len = new_population.first.length
+      chromosome_len = best_child.length
 
-      # Mutation process
-      mutated_count = CONFIG.genetic[:generational][:mutation_p] * new_population.length * chromosome_len
-      mutated_genes = (0 ... mutated_count).map do
-        @rng.rand(0 ... new_population.length * chromosome_len)
+      (0 ... chromosome_len).each do |i|
+        best_child.toggle_bit i  if @rng.rand < CONFIG.genetic[:stationary][:mutation_p]
+        worst_child.toggle_bit i if @rng.rand < CONFIG.genetic[:stationary][:mutation_p]
       end
-      mutated_genes.each do |gene|
-        chromosome = gene / chromosome_len
-        inner_gene = gene % chromosome_len
-        @endless_forms[chromosome].toggle_bit inner_gene
-      end
-
-      ## MISSING: Possibly mutate children (why?)
-      # Maybe we want to mutate the population *before* generating children?
 
       # Replacement scheme
       worst, worst_i = @endless_forms.each_with_index.min_by{ |c, i| fitness_for c }
-      second, second_i = @endless_forms.each_with_index.min_by{ |c, i| i == worst_i ? 1 : fitness_for c }
+      second, second_i = @endless_forms.each_with_index.min_by{ |c, i| (i == worst_i) ? 1 : (fitness_for c) }
 
-      if fitness_for best_child > fitness_for worst
-        if fitness_for worst_child > fitness_for worst
+      if fitness_for(best_child) > fitness_for(worst)
+        if fitness_for(worst_child) > fitness_for(worst)
           @endless_forms[worst_i] = worst_child
 
-          if fitness_for best_child > fitness_for second
+          if fitness_for(best_child) > fitness_for(second)
             @endless_forms[second_i] = best_child
           else
             # We would want to keep the best child, right?
@@ -44,7 +35,7 @@ module FeatureSelection
     end
 
     def evolution
-      (crossover selection 2).sort_by{ |c| fitness_for c }
+      (crossover *(selection 2)).sort_by{ |c| fitness_for c }
     end
   end
 end
