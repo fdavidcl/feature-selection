0a835438a074b3d9c63f1242546768518e7044f9
David Charte <fdavidcl@outlook.com>
2016-06-01 23:56:24 +0200

    Unify local searches into tools module

--- lib/feature-selection/heuristics/local_search.rb
+++ lib/feature-selection/heuristics/local_search.rb
@@ -1,6 +1,8 @@
 require_relative "heuristic"
 
 module FeatureSelection
+  # Abstract the basics of a local search in a module so we can hybridize other
+  # techniques later
   module LocalTools
     def neighborhood
       Enumerator.new do |yielder|
@@ -17,17 +19,37 @@ module FeatureSelection
       end
     end
 
+    def basic_local_search
+      next_one = [@solution, @fitness]
+      until next_one.nil?
+        # Save previous valid solution and find the next
+        self.solution = next_one
+        begin
+          next_one = select_next
+        rescue ArgumentError
+          next_one = nil
+        end
+      end
+    end
+
     # First descent selection
-    def select_next
+    def next_first_descent
       neighborhood.lazy.take(@max_evaluations - @evaluations).detect do |attempt, fitness|
         fitness > @fitness
       end
     end
+
+    # Max descent selection
+    def next_max_descent
+      neighbor, fitness = neighborhood.lazy.take(@max_evaluations - @evaluations).max_by &:last
+      [neighbor, fitness] if !neighbor.nil? && fitness > @fitness # returns nil otherwise
+    end
   end
 
+  # Build a class wrapper around the local search module so we can use it as an heuristic
   class LocalSearch < Heuristic
     include LocalTools
-    
+
     def initialize dataset, debug: false, random: Random.new(CONFIG.random_seed)
       super
 
@@ -59,18 +81,14 @@ module FeatureSelection
     def outer_loop
       basic_local_search
     end
+  end
 
-    def basic_local_search
-      next_one = [@solution, @fitness]
-      until next_one.nil?
-        # Save previous valid solution and find the next
-        self.solution = next_one
-        begin
-          next_one = select_next
-        rescue ArgumentError
-          next_one = nil
-        end
-      end
-    end
+  # Specialized classes for each kind of selection
+  class MaximumDescent < LocalSearch
+    alias_method :select_next, :next_max_descent
+  end
+
+  class FirstDescent < LocalSearch
+    alias_method :select_next, :next_first_descent
   end
 end
